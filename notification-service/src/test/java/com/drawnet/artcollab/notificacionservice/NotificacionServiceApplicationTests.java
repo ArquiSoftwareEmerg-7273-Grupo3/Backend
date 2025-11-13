6c3266ddb2b6f86705f96099373009617184306c
Juan Eduardo Montes Figueroa <u202210775@upc.edu.pe>
2025-11-12 19:23:37 -0500

    feat: Bounded Notification Implement

---
+++ notification-service/src/test/java/com/drawnet/artcollab/notificacionservice/NotificacionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drawnet.artcollab.notificacionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
